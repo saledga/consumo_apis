faafa1564cd35613082ba44cca3268099b5720d7
Juan Esteban Mesa <practicante.si2@comware.com.co>
2024-08-30 09:22:49 -0500

    feat(PROJECT): Component Pokemon
    :sparkles: endpoints are called by id and next and previous buttons are created to bring all pokemon visually.

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -3,20 +3,32 @@ import React, { useEffect, useState } from 'react'
 function Pokemon() {
 
   const [pokemon, setPokemon] = useState();
+  const [id, setId] = useState(1);
+
+  const handlePrevious = () => { 
+    setId(id - 1);
+  };
+
+  const handleNext = () => { 
+    setId(id + 1);
+  };
 
   useEffect(() => {
-    fetch(" https://pokeapi.co/api/v2/pokemon/1")
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then((resp) => resp.json())
     .then((data) => {
        setPokemon(data);
     })
-  }, [])
+  }, [id])
   
   return (
     <div>{pokemon &&
       <div>
         <h2>{pokemon.name}</h2>
         <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        <br />
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
       </div>}</div>
   )
 }
